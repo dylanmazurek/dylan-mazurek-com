42f7094223fbc471e94b652645b3d8aa4484cdf3
Dylan Mazurek <dylmazurek@gmail.com>
2025-01-26 00:28:15 +1100

    Refactor footer layout to use grid system and improve accessibility with span elements for link text

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,30 +3,26 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <footer className="row-start-3 grid grid-cols-2 sm:grid-cols-4 gap-6 text-lg">
             <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://bsky.app/profile/dylanmazurek.com"
-                target="_blank">
+                href="https://bsky.app/profile/dylanmazurek.com" target="_blank">
                 <Image src="/bluesky.svg" alt="Bluesky" width={20} height={20} />
-                Bluesky
+                <span>Bluesky</span>
             </Link>
             <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://www.linkedin.com/in/dylanmazurek"
-                target="_blank">
+                href="https://www.linkedin.com/in/dylanmazurek" target="_blank">
                 <Image src="/linkedin.svg" alt="LinkedIn" width={20} height={20} />
-                LinkedIn
+                <span>LinkedIn</span>
             </Link>
             <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://facebook.com/dylanmazurek"
-                target="_blank">
+                href="https://facebook.com/dylanmazurek" target="_blank">
                 <Image src="/facebook.svg" alt="Facebook" width={20} height={20}/>
-                Facebook
+                <span>Facebook</span>
             </Link>
             <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://instagram.com/dylanmazurek"
-                target="_blank">
+                href="https://instagram.com/dylanmazurek" target="_blank">
                 <Image src="/instagram.svg" alt="Instagram" width={20} height={20} />
-                Instagram
+                <span>Instagram</span>
             </Link>
         </footer>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ const domine = Domine({ subsets: ['latin'], display: "swap" });
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20">
             <main className="flex flex-col row-start-2 items-center sm:items-start">
-                <h1 className={`text-5xl font-bold mb-10 ${domine.className}`}>Engineer & <br/>Theatre Performer</h1>
-                <p className="text-center sm:text-left">
+                <h1 className={`text-5xl font-bold mb-10 ${domine.className}`}>Engineer &<br/><span className="animate-wiggle">Theatre Performer</span></h1>
+                <p className="text-lg">
                     Hi! I&apos;m Dylan Mazurek, a software engineer and musical theatre performer based in Melbourne, Australia.
                 </p>
                 {/* <Posts /> */}
